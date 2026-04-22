cebf5f3b7cd11f6aad9c3dc79fd95bf564072cc3
Mario Pellegrino Ambrosone <marioambrosone03@gmail.com>
2026-04-22 12:27:56 +0200

    Definition of ChannelModel module contract (i/o parameters) and initial documentation

--- src/ChannelModel/main_channel_function.m
+++ src/ChannelModel/main_channel_function.m
@@ -1,35 +1,62 @@
 %% Main Function Channel Model Module
 % The scope of this function is to call and execute all the channel model 
 % module functions that implement: (i) user behavior, (ii) satellite
-% constellation, (iii) diplay of the globe with users and satellites, (iv)
-% ... (TODEFINE)
+% constellation, (iii) reduce the constellation to the relevant coordinates,
+% (iv) display the globe with users and satellites, (v) user-satellite
+% links overtime.
 
-% FOR THE MOMENT THE FUNCTION HAS NO INPUT PARAMETERS %
+%%%%%% ----- INPUT PARAMETERS ----- %%%%%%
+% The main function obtains from the NAPOLEON_plus_1() function, therefore,
+% potentially, from the GUI as the end-user choose different values, the 
+% following data
+%       1.  numUsers: number of users in a selected range
+%       2.  startTime: initial time of simulation
+%       3.  stopTime: finish time of simulation
+%       4.  sampleTime: T_sampling for the computation of the satellite's
+%       position and link state parameters
 
 %%%%%% ----- OUTPUT PARAMETERS ----- %%%%%%
 % The main function outputs:
-%       1. SATELLITE_Set: Data Structure containing the satellite objects
+%       1.  filteredSatellite_Set : Data Structure containing the satellite objects
 %       relevant for the AoI simulated
 %
-%       2. USERS_Set: Data Structure containing the set of users with (i) 
+%       2.  USERS_Set: Data Structure containing the set of users with (i) 
 %       their position (coordinates), (ii) the kind of environment they are
-%       in, (iii) ??? Possibly, if it is a tensor, the time evolution
-%       User-Sat set
+%       in
+% 
+%       3.  USER_SAT_evolution: Array of Data Structures containing the time
+%       evolution of the channel's parameters of each user-satellite link.
 
-function []=main_channel_function()
+function [SATELLITE_Set, USERS_Set, USER_SAT_evolution]=main_channel_function(numUsers, startTime, stopTime, sampleTime)
 
 % Init output structures
+filteredSatellite_Set = struct();
 SATELLITE_Set = struct();
 USERS_Set = struct();
+USER_SAT_evolution = struct();
+
+% Constellation fixed parameters. We model a Starlink constellation, namely
+% the first shell constellation according to the FCC 21-48 documentation.
+% We note that
+
+% Init user parameters.  We assume a small italy-centric portion of europe,
+% in which a multi distributed user behavior is assumed. The
+% users are static (mobile speed = 0) for simplicity. The user may as well
+% be though of as base stations, hence antennas.
+
+% CALL SATELLITE FUNCTION - defines the Starlink shell 1 constallation
+SATELLITE_Set = Satellite_constellation(); 
 
 % CALL USER FUNCTION
-USERS_Set = User_behavior(); % Assuming a function to initialize user data
+USERS_Set = User_behavior(); 
 
-% CALL SATELLITE FUNCTION
-SATELLITE_Set = Satellite_constellation(); % Assuming a function to initialize satellite data
+% REDUCING SATELLITAR OBJECTS TO USER-ONLY RELEVANT SATELLITES
+filteredSatellite_Set = Filter_constellation(SATELLITE_Set, USERS_Set); %
 
 % CALL DISPLAY FUNCTION
-Display_globe(SATELLITE_Set, USERS_Set); % Assuming a function to visualize the globe with users and satellites
+Display_globe(filteredSatellite_Set, USERS_Set); 
 
+% COMPUTATION OF THE SATELLITAR LINK STATISTICS AND CHANNEL SIMULATION
+USER_SAT_evolution = channel_model();
 
 end
--- src/NAPOLEON_plus_1.m
+++ src/NAPOLEON_plus_1.m
@@ -9,3 +9,27 @@
 %
 % Main Script responsible for the execution and call of all the other
 % modules main functions. 
+
+clc; close all; clear
+
+%%%%%%% ------- SCENARIO SETTINGS ------ %%%%%%%
+% We fix the simulation time to the orbital period od a single satellite.
+% Regarless of its position with respect to a given user, to avoid
+% one-day-long simulation with 15 satellitar orbits
+
+startTime  = datetime('today');
+stopTime   = startTime + hours(1) + minutes(36);
+sampleTime = 60; % seconds, maybe less in future
+
+
+%%%%%% ------ GUI DEPENDANT QUANTITIES ----- %%%%%%
+% From the GUI the end-user is able to fix the number of simulated users/gb
+% as an input parameter, either from a pool of available values or freely, in the
+% order of ten, maximum a hundred for the sake of computational
+% complexity at run time and correct functioning of the simulator
+
+numUsers = 100; % Example number of users, TODO GUI. Momentarily hard-coded
+% When the GUI will be implemented, an exception management shall be
+% developed: either the end-user can choose numUsers from a pool of
+% available values or, if numUsers is over a certain range, it will be
+% asked to set a new value
