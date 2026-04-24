4cb1ba996be7ffd667c9de62eaa1b4aff44bfe9c
Mario Pellegrino Ambrosone <marioambrosone03@gmail.com>
2026-04-24 17:12:13 +0200

    Added all functions with documentation channel model module non final version

--- src/ChannelModel/Display_globe.m
+++ src/ChannelModel/Display_globe.m
@@ -1,4 +1,11 @@
-function []=Display_globe()
+%% Display_globe function
+% This function displays the 3D view of the globe, highligthing the users
+% and the satellites after filtering
+
+%%%%%% ----- INPUT PARAMETERS ----- %%%%%%
+%       1. filteredSimScen: filtered satelliteScenario object of the toolbox
+
+function []=Display_globe(filteredSimScen)
 
 
 end
--- src/ChannelModel/Satellite_constellation.m
+++ src/ChannelModel/Satellite_constellation.m
@@ -1,3 +1,27 @@
-function [SATELLITE_Set]=Satellite_constellation()
+%% Satellite_constellation function
+% This function computes the whole First Shell Starlink Constellation in
+% compliance with the FCC 21-48 authorization. That is, the 540km altitude, 
+% 72 orbital planes, 22 satellites per orbital plane, constellation. 
+
+%%%%%% ----- INPUT PARAMETERS ----- %%%%%%
+%       1. configConst : Data Strcuture containing the parameters needed
+%       to configurate the constellation:
+%                   (i) the number of orbital planes
+%                   (ii) the number of satellite per planes
+%                   (iii) the altitude of the constellation
+%                   (iv) the inclination with respect to the equatorial 
+%                       line of the planes
+%                   (v) the phasing parameter to offset satellites on
+%                       different planes
+%
+%       2. simulationScenario : satelliteScenario object of the toolbox to
+%       be updated with the Satellite objects
+%
+
+%%%%%% ----- OUTPUT PARAMETERS ----- %%%%%%
+%       1. simulationScenario : updated simulationScenario where the satellites
+%       can be accessed as Satellites object
+
+function [simulationScenario]=Satellite_constellation(configConst, simulationScenario)
 
 end
