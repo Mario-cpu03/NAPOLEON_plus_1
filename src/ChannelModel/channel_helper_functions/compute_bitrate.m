40f9c8dafe77442dd884b53043ca2b59dd8c1ead
AdityaG135 <adityagohite@gmail.com>
2026-05-02 21:06:55 +0200

    Adding functions for snr and bitrate
    This reverts commit 5860c3b3d76ec8177c1897a7654084445b7e1298.

--- src/ChannelModel/channel_helper_functions/compute_bitrate.m
+++
@@ -1,3 +0,0 @@
-function bitrate_tensor = compute_bitrate(snr_tensor, B)  %B is the bandwidth
-         bitrate_tensor = B.*log(1+snr_tensor); 
-end
--- src/ChannelModel/channel_helper_functions/compute_snr.m
+++
@@ -1,5 +0,0 @@
-%The Gain for the transmition and receiver are the linear quantities.
-function snr_tensor = compute_snr(pathgain_tensor, configChannel)
-         snr_tensor = (configChannel.P_sat_lin * configChannel.G_sat_lin * configChannel.G_u_lin .* pathgain_tensor)/configChannel.N_0;
-
-end
