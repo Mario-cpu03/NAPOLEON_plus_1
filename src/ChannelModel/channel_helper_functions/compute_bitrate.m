102cc95a70d8587e943cf6c05d9d5f2c08d10b32
Mario Pellegrino Ambrosone <marioambrosone03@gmail.com>
2026-05-07 19:44:48 +0200

    Elimination files for refactor of the whole channel model. Following a trace based approach

--- src/ChannelModel/channel_helper_functions/compute_bitrate.m
+++
@@ -1,5 +0,0 @@
-function bitrateTensor = compute_bitrate(snrTensor, configChannel)
-
-bitrateTensor = configChannel.channel_bandwidth .* log2(1 + snrTensor);
-
-end
--- src/ChannelModel/channel_helper_functions/compute_latency.m
+++
@@ -1,5 +0,0 @@
-function latency = compute_latency(distanceTensor, c)
-
-latency = distanceTensor ./ c;
-
-end
