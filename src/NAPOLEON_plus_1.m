5a6ee72cc5284d96c13cd213726fbd781098b484
laspinasanti <laspinasanti@gmail.com>
2026-05-31 13:22:50 +0200

    Update files

--- src/NAPOLEON_plus_1.m
+++ src/NAPOLEON_plus_1.m
@@ -10,6 +10,7 @@
 % modules main functions. 
 
 close all; clc; clear
+rng(13); %%Seed for reproducibility
 
 %%%%%%% ------- MODULES VISIBILITY SETTINGS ------ %%%%%%%
 % We make the path of each module, hence each directory, visible from the
@@ -42,7 +43,7 @@ sampleTime = 20; % seconds
 % order of ten, maximum a hundred for the sake of computational
 % complexity at run time and correct functioning of the simulator
 
-numUsers = 10; % Example number of users, TODO GUI. Momentarily hard-coded
+numUsers = 300; % Example number of users, TODO GUI. Momentarily hard-coded
 % When the GUI will be implemented, an exception management shall be
 % developed: either the end-user can choose numUsers from a pool of
 % available values or, if numUsers is over a certain range, it will be
@@ -54,12 +55,81 @@ numUsers = 10; % Example number of users, TODO GUI. Momentarily hard-coded
 mode = "forecast"; %mode = "ideal";
 
 % From the GUI the end-user is able to select the nature of the association
-% alroithm from a pool of pre-defined policies. Another possible choice is
-% to enable the user to manually select the amount of "reward" and "penalization"
-% of each relevant quantity. The latter of the two options requires an
-% higher end-user knowledge but offers more flexibility; on the other hand,
-% the first option may be easier to develop but more expensive to compute, plus
-% it requires hard-coded weights and fixed kinds of algorithms. 
+% alroithm from a pool of two pre-defined IMT-2020 inspired policies:
+%   (i) URLLC-based algorithm, for a low latency (minimum distance)
+%   Hand-over aware algorithm.
+%
+%   (ii) eMBB-based algorithm, for a maximum throughput, minimum HO
+%   algorithm.
+association_algorithm = "URLLC"; % association_algorithm = "eMBB"
+
+
+%%%%%% ------ CONFIGURATION DATA STRUCTURES ----- %%%%%%
+
+% CONFIG structure for the constellation deployment. The parameters
+% chosen are to be found on paragraph 4 chapter 2 of the FCC 21-48.
+configConst = struct( ...
+              'planes', 72, ... % Total number of orbital planes, integer variable, must be reduced in Filter_constellation 
+              'satPlanes', 22, ... % Total number of satellites per plane, integer variable, most probably will not be reduced in Filter_constellation 
+              'inclination', 53.2, ... % Equatorial inclination of the orbit, float variable, .2 degrees more inclinated than 2020 constellation
+              'phasingParam', 17, ... % Phasing param multiplying phasing offset obtained in Phasing Parameter Analysis for Satellite Collision Avoidance in Starlink and Kuiper Constellations
+              'altitude', 540); % Altitude of the (second) shell
+
+% CONFIG structure for the AoI and BS deployment.
+% We assume a small portion of earth focused on the continental europe 
+% in which a multi distributed user behavior is assumed. 
+% The users are static (mobile speed = 0) for simplicity. 
+% The user may as well be though of as base stations, hence antennas.
+configAoI = struct( ...
+            'latMin', 35, ...
+            'latMax', 60, ...
+            'lonMin', -10, ...
+            'lonMax', 30, ...
+            'deltaLat', 2, ...
+            'deltaLon', 2);
+
+% CONFIG minimum elevation threshold for satellite filtering and 
+% weight normalization according to FCC 21-48 documentation.
+minimumElev = 25;
+
+% CONFIG channel parameters for ChannelModel 
+% Those parameters are selected in compliance with the ITU-R P.681-10, but
+% not in full consistency with the Starlink shells, whose operation bands
+% are the Ku and Ka band.
+k_B = 1.380649e-23; %Boltzmann konstant
+T_sys = 290; %std teemparature for noise computation
+B = 5e6; % bandwidth 5MHz 
+
+configChannel = struct( ...
+    'P_sat_lin', 1, ... % power of the signal, one watt as a starting base, may be varied if needed 
+    'G_sat_lin', 10^(50/10), ... %gain of the satellite antenna
+    'G_u_lin', 10^(0/10), ... %0dBi of gain for the user assuming isotropic antenas
+    'N_0', k_B*T_sys*B, ... %noise power
+    'channel_bandwidth', B, ... %bandwidth of the system on each channel
+    'carrierFrequency', 2e9, ... %itu-r aligned carrier
+    'mobileSpeed', 5000/3600, ... % assuming a 5km/h speed to obtain doppler shift: v=1.389m/s --> f_Dmobile = v*f_c/c = 9.2593 Hz approx 10Hz
+    'sampleRate', 100, ... %see note below
+    'traceLengthSamples', 2000, ... %number of samples obtained as the channel sample rate times the sample time of the simulator: 100[1/s]*20[s] = 2000
+    'CSImode', mode); %mode of the channel. See channel_model for more information
+%%NOTE on the sample rate:
+% p681LMSChannel requires fD_mobile + |fD_sat| < f_sampling/10.
+% Here fD_sat = 0 by default, thus for fD_mobile = 9.26567 Hz,
+% we have that f_samling must be > 92.6567 Hz.
+
+% CONFIG association parameters needed for the compute_base_cost_tesnor
+% function for normalization purposes.
+configAssociation = struct( ...
+    'association_algorithm', association_algorithm, ...
+    'carrierFrequency', configChannel.carrierFrequency, ...
+    'P_sat_lin', configChannel.P_sat_lin, ...
+    'G_sat_lin', configChannel.G_sat_lin, ...
+    'G_u_lin', configChannel.G_u_lin, ...
+    'channel_bandwidth',configChannel.channel_bandwidth, ...
+    'N_0', configChannel.N_0, ...
+    'minimumElev', minimumElev, ...
+    'altitude_satellites', configConst.altitude, ...
+    'altitude_groundStation', 0, ... %we assume sea-level for simplicity
+    'G', 2); % capacity constraint for load balancing on the satellites
 
 
 %%%%%% ------ CHANNEL MODEL MODULE EXECUTION ----- %%%%%%
@@ -73,14 +143,26 @@ mode = "forecast"; %mode = "ideal";
 %           quantities by the UserSatAssoc module (time vector, number of users,
 %           number of satellites and number of time samples)
 
-[USER_SAT_evolution]=main_channel_function(numUsers, startTime, stopTime, sampleTime, mode);
+[USER_SAT_evolution]=main_channel_function(numUsers, startTime, stopTime, sampleTime, configConst, configAoI, configChannel, minimumElev);
 
 
-%%%%%% ------ USER-SATELLITE ASSOCIATION MODULE EXECUTION ----- %%%%%%
-% We call the user_sat_assoc_function to associate users with satellites
-% based on the USER_SAT_evolution data structure obtained from the channel model.
+%%%%%% ------ USER SATELLITE ASSOCIATION MODULE EXECUTION ----- %%%%%%
+% We call the main_association_function to obtain the history of association
+% outcomes, dictated by each algorithm, between user and
+% satellites.
+% 
+% The USER_SAT_association datastracture is the object of
+% relevance and it is organized as:
+%           (i) TO DEFINE ....
+
+[USER_SAT_association]=main_association_function(USER_SAT_evolution, configAssociation);
 
 
-association_algorithm="URLLC";   %association_algorithm="eMBB";
 
-%[USER_SAT_association] = main_association_function(USER_SAT_evolution, numUsers, association_algorithm);
+
+%%%%%% ------ KPI MODULE EXECUTION ----- %%%%%%
+% We call the main_KPI_function to obtain the evaluation by means of
+% KeyPerformanceIndicators of the association algorithm that the end-user
+% has selected when interacting with the simulator
+
+%KPI_results = main_KPI_function(USER_SAT_association, USER_SAT_evolution, mode, );
--- src/UserSatAssoc/main_association_function.m
+++ src/UserSatAssoc/main_association_function.m
@@ -13,35 +13,15 @@
 
 %%%%%% ----- INPUT PARAMETERS ----- %%%%%%
 %       1. USER_SAT_evolution : Data Structure containing metadata and
-%       tensor quantities required by the UserSatAssoc module:
+%       tensor quantities required by the UserSatAssoc module
 %
-%                   (i)   timeVec : simulation time vector
-%                   (ii)  validLinkMask : logical tensor [U x S x T].
-%                         true where user u can see satellite s at time t
-%                         and the link has been processed by the channel
-%                         model
-%                   (iii) SNRtensor : tensor [U x S x T] containing the
-%                         linear SNR associated with each valid link
-%                   (iv)  rateTensor : tensor [U x S x T] containing the
-%                         achievable rate [bit/s] associated with each
-%                         valid link
-%                   (v)   pathGainTensor : tensor [U x S x T] containing
-%                         the real nonnegative power gain obtained by
-%                         combining FSPL gain and LMS fading power
-%                   (vi)  distanceTensor : tensor [U x S x T] containing
-%                         slant distance [m]. Propagation latency can be
-%                         computed later from this quantity as d/c
-%                   (vii) elevationClassTensor : tensor [U x S x T]
-%                         containing the quantized elevation class assigned
-%                         to each valid link
-%                   (viii) numUsers : number of users
-%                   (ix)  numSats : number of satellites
-%                   (x)   numTimeSteps : number of simulation time samples
+%       2. association_algorithm : string describing the algorithm chosen
+%       by the end user, needed for normalization purposes (see documentation 
+%       of compute_base_cost_tensor into the munkres_helper_functions folder)
 
 %%%%%% ----- OUTPUT PARAMETERS ----- %%%%%%
 %           Data structure containing the association between the users with a satellite along time istants. 
-%           Furthermore, the output data structure should contains all the datas required for the KPI module. 
-%           For example: 
+%           Furthermore, the output data structure should contains all the data required for the KPI module. 
 %           1. USER_SAT_association : Data Structure containing metadata and
 %           tensor quantities required by the KPI module:
 %                   (i)     associationTensor:tensor [U x S x T] containing the
@@ -57,58 +37,41 @@
 %                       THE KPI FUNCTION THAT WILL BE IMPLEMENTED
 
 
-%%   THINGS TO FIX:
-%                   1) Input arguments for the compute_base_cost_tensor function (see comments inside the function)
-%                   1) configChannel Should not be defined here, but should
-%                   be given as input parameter of the main_association_function (then tocompute_base_cost_tensor function)
-%   
-%%
+function [USER_SAT_association]=main_association_function(USER_SAT_evolution, configAssociation)
 
-
-
-function [USER_SAT_association]=main_association_function(USER_SAT_evolution, numUsers, association_algorithm)
-
-    addpath('UserSatAssoc/Munkres helper functions'); %helper functions for the construction of the weights matrix    
-    
-    %%              --- SHOULD NOT BE THERE, JUST FOR DEBUGGING---
-    
-    %We need this since the compute_base_cost_tensor functions normalizes the
-    %values with theoretical quantities that depends on how we defined the
-    %simulation scenario (see comments inside the function).
-    k_B = 1.380649e-23; %Boltzmann konstant
-    T_sys = 290; %std teemparature for noise computation
-    B = 5e6; % bandwidth 5MHz 
-    
-    configChannel = struct( ...
-        'P_sat_lin', 1, ... % power of the signal, one watt as a starting base, may be varied if needed 
-        'G_sat_lin', 10^(50/10), ... %gain of the satellite antenna
-        'G_u_lin', 10^(0/10), ... %0dBi of gain for the user assuming isotropic antenas
-        'N_0', k_B*T_sys*B, ... %noise power
-        'channel_bandwidth', B, ... %bandwidth of the system on each channel
-        'carrierFrequency', 2e9, ... %itu-r aligned carrier
-        'mobileSpeed', 5000/3600, ... % assuming a 5km/h speed to obtain doppler shift: v=1.389m/s --> f_Dmobile = v*f_c/c = 9.2593 Hz approx 10Hz
-        'sampleRate', 100, ... %see note below
-        'traceLengthSamples', 2000, ... %number of samples obtained as the channel sample rate times the sample time of the simulator: 200[1/s]*20[s] = 4000
-        'CSImode', mode); %mode of the channel. See channel_model for more information
-    
-    %%
-
+    addpath('UserSatAssoc/munkres_helper_functions/'); %helper functions for the construction of the weights matrix    
 
     %Here we construct the weight matrix. Since it will be computational costly to buit it time step per time step, here we build a cost tensor without taking into account the handover penalty.  
     %The parameter that models the handover penalty will be give as output of the function. That will be consider when we call the Munkres time instant per time instant. 
     %In this way we optimize as much as possible the computational cost. 
     %As it is defined, the base_cost_tensor has values in range [0,2) (limit case that actually will never happend), not taking into account the handover penalization.
     %Actual values will be in range [0,1.#]
-    
-    [base_cost_tensor,handover_weight]=compute_base_cost_tensor(USER_SAT_evolution,association_algorithm, configChannel);    
+    association_algorithm = configAssociation.association_algorithm;
+
+    [base_cost_tensor,handover_weight]=compute_base_cost_tensor(USER_SAT_evolution, configAssociation);    
         
     [U, S, T] = size(base_cost_tensor);
     
-    G=2;                         % max number of possible connections per satellite. HARD CODED HERE. SHOULD BE PASSED AS AN INPUT FOR THE FUNCTION
-        
-    USER_SAT_association.associationTensor = false(U, S, T);    %Initialization of the output structure
-    prev_association = false(U, S);                             %Inizialization of the matrix that we use to consider the handover penalization strategy
+    G=configAssociation.G;                   % max number of possible connections per satellite. HARD CODED HERE. SHOULD BE PASSED AS AN INPUT FOR THE FUNCTION
+      
+    %%OUTPUT INIT VIA MATRIX APPROACH
+    USER_SAT_association.assignedSatIdx = NaN(U, T);
+    USER_SAT_association.rate_bps  = NaN(U, T);
+    USER_SAT_association.SNR_lin = NaN(U, T);
+    USER_SAT_association.distance_m = NaN(U, T);
+    USER_SAT_association.latency_s = NaN(U, T);
+    USER_SAT_association.handoverEvent = false(U, T);
+
+    % DEBUGGING PURPOSES STRUCTURE
+    % This logical matrix is used only to check whether each user is
+    % actually served at each time step. It is independent from the simulator
+    % logic: removing it does not affect the association algorithm, the
+    % Munkres optimization, or the handover computation.
+    USER_SAT_association.servedMask = false(U, T);
     
+    prev_association = false(U, S);
+    
+    c = 3e8;  
     for t = 1:T
     
         real_sat_idx = zeros(U, 1);                                 %inizialization of the vector that will contains the real indices of the associations  
@@ -140,32 +103,8 @@ function [USER_SAT_association]=main_association_function(USER_SAT_evolution, nu
         expanded_cost_matrix = repelem(reduced_cost_matrix, 1, G);          %replace every columns G times
         num_virtual_sats = size(expanded_cost_matrix, 2);
     
-    
-        %Munkres works with square matrix, so we have to insert "dummy" rows in
-        %the expanded_cost_matrix. 
-        num_dummies = num_virtual_sats - U;    
-    
-        if num_dummies > 0
-
-            %if there are more visible slots than users, we add num_dummies rows
-            %to the expanded_cost_matrix. Every row has cost 0
-            dummy_matrix = zeros(num_dummies, num_virtual_sats);
-            square_cost_matrix = [expanded_cost_matrix; dummy_matrix];
-
-        elseif num_dummies < 0
-
-            %if there are more users that visible slots.
-            %almost impossible case, only to be sure that the munkress will always work. 
-            dummy_cols = Inf(U, abs(num_dummies));
-            square_cost_matrix = [expanded_cost_matrix, dummy_cols];
-
-        else
-            square_cost_matrix = expanded_cost_matrix;   
-        end
-    
-    
         %now we havo to perform the munkres
-        assignment = munkres(square_cost_matrix);     %munkres function will return a row vector with dimensions [1 x U]. 
+        assignment = munkres(expanded_cost_matrix);     %munkres function will return a row vector with dimensions [1 x U]. 
                                                       %the value for every index represent the index of the assigned satellite for that user
 
         %Munkres will return also the assignment for the dummy users. We filter out that associations.
@@ -180,10 +119,10 @@ function [USER_SAT_association]=main_association_function(USER_SAT_evolution, nu
         %check. 
         if any(valid_users)
             % Troviamo le coordinate (Riga, Colonna) solo per gli utenti attualmente 'true'
-            idx_cost = sub2ind(size(square_cost_matrix), find(valid_users), virt_sat_idx(valid_users));
+            idx_cost = sub2ind(size(expanded_cost_matrix), find(valid_users), virt_sat_idx(valid_users));
             
             % Troviamo quali di queste connessioni sono fisicamente impossibili (Inf)
-            is_inf = square_cost_matrix(idx_cost) == Inf;
+            is_inf = expanded_cost_matrix(idx_cost) == Inf;
             
             % Selezioniamo gli indici degli utenti validi e li forziamo a 'false' se il costo è Inf
             valid_users_indices = find(valid_users);
@@ -191,19 +130,80 @@ function [USER_SAT_association]=main_association_function(USER_SAT_evolution, nu
         end
 
         if any(valid_users)
+        
             % Map virtual satellite index -> visible satellite index
-            slot_idx = ceil(virt_sat_idx(valid_users) / G);       %Ex: ceil(15/2)=8;  
-
+            slot_idx = ceil(virt_sat_idx(valid_users) / G);
+        
             % Map visible satellite index -> real satellite index
             real_sat_idx(valid_users) = visible_sats_indices(slot_idx);
-
-            % Build boolean association matrix
-            lin_idx = sub2ind([U, S], find(valid_users), real_sat_idx(valid_users));
-            current_association(lin_idx) = true;
-        end    
         
-    
-        USER_SAT_association.associationTensor(:,:,t) = current_association;
+            % Actual users associated at this time step
+            user_idx = find(valid_users);
+        
+            % Build boolean association matrix.
+            % This is still needed internally for the handover penalty at the next
+            % time step, but it is no longer stored over all time steps.
+            lin_idx = sub2ind([U, S], user_idx, real_sat_idx(valid_users));
+            current_association(lin_idx) = true;
+        
+            % Linear indices into the original [U x S x T] channel tensors
+            lin_idx_tensor = sub2ind( ...
+                [U, S, T], ...
+                user_idx, ...
+                real_sat_idx(valid_users), ...
+                t * ones(numel(user_idx), 1));
+        
+            % Compact output storage
+            USER_SAT_association.assignedSatIdx(user_idx, t) = real_sat_idx(valid_users);
+
+            % DEBUGGING PURPOSES STRUCTURE
+            % Mark the users that received a valid selected link at this
+            % time step. This is only used to distinguish "no handover
+            % because the satellite did not change" from "no handover
+            % because the user was not served".
+            USER_SAT_association.servedMask(user_idx, t) = true;
+        
+            USER_SAT_association.rate_bps(user_idx, t) = ...
+                USER_SAT_evolution.rateTensor(lin_idx_tensor);
+        
+            USER_SAT_association.SNR_lin(user_idx, t) = ...
+                USER_SAT_evolution.SNRtensor(lin_idx_tensor);
+        
+            USER_SAT_association.distance_m(user_idx, t) = ...
+                USER_SAT_evolution.distanceTensor(lin_idx_tensor);
+        
+            USER_SAT_association.latency_s(user_idx, t) = ...
+                USER_SAT_association.distance_m(user_idx, t) / c;
+        
+        end
+        
+        if t > 1
+        
+            prevSat = USER_SAT_association.assignedSatIdx(:, t-1);
+            currSat = USER_SAT_association.assignedSatIdx(:, t);
+        
+            bothServed = ~isnan(prevSat) & ~isnan(currSat);
+        
+            USER_SAT_association.handoverEvent(:, t) = bothServed & (currSat ~= prevSat);
+        
+        end
+        
         prev_association = current_association;
     end
+
+    USER_SAT_association.numUsers = U;
+    USER_SAT_association.numSats = S;
+    USER_SAT_association.numTimeSteps = T;
+    USER_SAT_association.association_algorithm = association_algorithm;
+
+    USER_SAT_association.timeVec = USER_SAT_evolution.timeVec;
+    
+    USER_SAT_association.totalHandoversPerUser =sum(USER_SAT_association.handoverEvent, 2);
+    
+    USER_SAT_association.totalHandoversSystem = sum(USER_SAT_association.totalHandoversPerUser);
+
+    % DEBUGGING PURPOSES STRUCTUREs
+    USER_SAT_association.servedRatioPerUser = mean(USER_SAT_association.servedMask, 2);
+    USER_SAT_association.servedRatioSystem = mean(USER_SAT_association.servedMask(:));
+
 end
